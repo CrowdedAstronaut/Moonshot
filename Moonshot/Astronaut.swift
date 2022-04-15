1cd6abdc9fff3e9817af228c1696b7796545ae93
CrowdedAstronaut <ross@johnrossphillips.com>
2022-04-15 09:29:20 -0500

    Formatting our mission view

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,13 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Ross Phillips on 4/15/22.
+//
+
+import Foundation
+struct Astronaut: Codable, Identifiable {
+	let id: String
+	let name: String
+	let description: String
+}
